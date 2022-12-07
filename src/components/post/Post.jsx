637f6831b336d2ff1f032986cc182d149aa84c0f
imak02 <asbinkhanal222@gmail.com>
2022-12-07 22:18:09 +0545

    Add posts section

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,48 @@
+import "./post.scss";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import { Link } from "react-router-dom";
+
+const Post = ({ post }) => {
+  const liked = false;
+
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="profile" />
+            <div className="details">
+              <Link className="link" to={`/profile/${post.userId}`}>
+                <span className="name">{post.name}</span>
+              </Link>
+              <span className="date">20 seconds ago</span>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={post.img} alt="post" />
+        </div>
+        <div className="features">
+          <div className="item">
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            12 Likes
+          </div>
+          <div className="item">
+            <TextsmsOutlinedIcon />2 Comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon /> 2 Shares
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,7 +1,51 @@
+import Post from "../post/Post";
 import "./posts.scss";
 
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  //Temporary data
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/14664638/pexels-photo-14664638.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. ex earum animi repellat nam assumenda tempore iusto unde, libero consequuntur tempora nisi inventore voluptatum.",
+      img: "https://images.pexels.com/photos/14664638/pexels-photo-14664638.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/14664638/pexels-photo-14664638.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. ex earum animi repellat nam assumenda tempore iusto unde, libero consequuntur tempora nisi inventore voluptatum.",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      userId: 3,
+      profilePic:
+        "https://images.pexels.com/photos/14664638/pexels-photo-14664638.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. ex earum animi repellat nam assumenda tempore iusto unde, libero consequuntur tempora nisi inventore voluptatum.",
+      img: "https://images.pexels.com/photos/14664638/pexels-photo-14664638.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      userId: 4,
+      profilePic:
+        "https://images.pexels.com/photos/14664638/pexels-photo-14664638.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. ex earum animi repellat nam assumenda tempore iusto unde, libero consequuntur tempora nisi inventore voluptatum.",
+    },
+  ];
+  return (
+    <div className="posts">
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
+    </div>
+  );
 };
 
 export default Posts;
